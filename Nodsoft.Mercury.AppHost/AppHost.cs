8a71ce5be21a510e53e35b76b41b2fa90b756d99
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2025-09-20 17:31:27 +0200

    feat: Add CosmosDB resource setup for cloud storage
    Introduce CosmosDB resources and containers setup for database integration, enabling serverless deployment with emulator support and connection to necessary data containers.

--- Nodsoft.Mercury.AppHost/AppHost.cs
+++ Nodsoft.Mercury.AppHost/AppHost.cs
@@ -1,3 +1,18 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Aspire.Hosting.Azure;
+
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+// Database: CosmosDB (serverless)
+#pragma warning disable ASPIRECOSMOSDB001
+IResourceBuilder<AzureCosmosDBResource> cosmos = builder.AddAzureCosmosDB("cosmosdb")
+	.RunAsPreviewEmulator(emulator =>
+	{
+		emulator.WithLifetime(ContainerLifetime.Persistent);
+		emulator.WithDataExplorer();
+	});
+
+IResourceBuilder<AzureCosmosDBDatabaseResource> formsDb = cosmos.AddCosmosDatabase("forms-db");
+IResourceBuilder<AzureCosmosDBContainerResource> formSources = formsDb.AddContainer("sources", "/id");
+IResourceBuilder<AzureCosmosDBContainerResource> forms = formsDb.AddContainer("forms", "/id");
 
 builder.Build().Run();
