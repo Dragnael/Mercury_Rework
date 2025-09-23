f636ee8bd04ac0ff7d8ab338b4295fb4f6d9488b
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2025-09-23 11:23:12 +0200

    feat: Add DTOs for form sources, tokens, and submissions
    Introduces data transfer objects to model form source details, access tokens, form submissions, and form templates, enhancing API structure and data management.

---
+++ Nodsoft.Mercury.Models/FormSourceDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nodsoft.Mercury.Models;
+
+/// <summary>
+/// Represents the DTO for a source of forms within the Mercury application.
+/// </summary>
+public sealed record FormSourceDto
+{
+	/// <summary>
+	/// The ID of the form source.
+	/// </summary>
+	public required Guid Id { get; set; } = Guid.CreateVersion7();
+	
+	/// <summary>
+	/// The name of the form source.
+	/// </summary>
+	/// <remarks>
+	/// This is typically the name of the application or module.
+	/// </remarks>
+	[StringLength(256)]
+	public required string Name { get; set; }
+	
+	/// <summary>
+	/// The URL of the form source.
+	/// </summary>
+	[Url]
+	public string? Url { get; set; }
+	
+	/// <summary>
+	/// A contact email address for the form source.
+	/// </summary>
+	[EmailAddress]
+	public required string ContactEmail { get; set; }
+}
